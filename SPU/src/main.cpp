b0bc895888e717f3ca6ec842985c416b922e5c01
Artiom_Fedora <Artiom.shaff07@gmail.com>
2025-02-07 08:32:40 +0300

    test commit

--- SPU/src/main.cpp
+++ SPU/src/main.cpp
@@ -5,12 +5,12 @@
 //-----------------------------------------------------------
 
 int main ()
-    {
+{
     FILE* machine_code = fopen ("./MachineCode.bin", "rb");
 
     Processor (machine_code);
 
     fclose (machine_code);
-    }
+}
 
 //-----------------------------------------------------------
